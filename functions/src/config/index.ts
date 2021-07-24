893e324ea56ef4fe3ca8e1406da9f9560f33bbb5
Rahul Sharma <90137rahul@gmail.com>
2021-07-24 10:54:06 +0530

    ♿️ Added sendgrid config

--- functions/src/config/index.ts
+++ functions/src/config/index.ts
@@ -1,13 +1,19 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 export default {
   /**
    * Jwt secret key
    */
   secret: process.env.JWT_SECRET_KEY,
+
+  /**
+   * Send Grid API Key
+   */
+  sendGrid: process.env.SEND_GRID_API_KEY,
+  templateId: process.env.SEND_GRID_TEMPLATE_ID,
 
   /**
    * Firebase Config files
