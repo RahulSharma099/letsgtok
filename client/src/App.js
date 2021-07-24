b59d89abfc746694448dd4bef59b4161f1e3f604
Rahul Sharma <90137rahul@gmail.com>
2021-07-24 19:08:20 +0530

    🚨 Added react toasties

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Footer from './components/Footer';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        pauseOnHover
+      />
       <Navbar />
       <Home />
       <Footer />
