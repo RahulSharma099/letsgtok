383e2a14e8d250d2bf4636ffb020caa214fe1eb0
Rahul Sharma <90137rahul@gmail.com>
2021-07-24 11:01:01 +0530

    💄 Added main react files

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
